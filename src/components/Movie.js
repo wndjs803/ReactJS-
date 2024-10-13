20967fbbe1a712acae16955ebd7e7d095adae521
김주언 <wndjs803@naver.com>
2024-10-13 21:25:22 +0900

    Movie app part2

---
+++ src/components/Movie.js
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+function Movie({ id, coverImg, title, summary, genres }) {
+  return (
+    <div key={id}>
+      <img src={coverImg} />
+      <h2>{title}</h2>
+      <p>{summary}</p>
+      <ul>
+        {genres.map((genre, index) => (
+          <li key={index}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
